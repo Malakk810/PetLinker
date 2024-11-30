465440e8efb4753936e0277cf33cdbc4dcf5b8d0
Malak Khaled <s-malak.mahmoud@zewailcity.edu.eg>
2024-11-30 20:16:37 +0200

    Update adoptionServices.py

--- adoptionServices.py
+++ adoptionServices.py
@@ -13,33 +13,34 @@ class AdoptionService:
         self.cursor = self.conn.cursor()
 
     def get_all_pets(self):
-        self.cursor.execute("SELECT pet_id, name, breed, status FROM pets WHERE status='Available'")
+        self.cursor.execute("SELECT Licence_number, Gender, Age, Health_Condition,Type_, Status_ FROM pets WHERE Status_='Available',Location")
         pets = self.cursor.fetchall()
-        return [{"id": row[0], "name": row[1], "breed": row[2]} for row in pets]
+        return [{"Licence_number": row[0], "Gender": row[1], "Age": row[2], "Health_condition": row[3],"Type_": row[4], "Status_": row[5]} for row in pets]
 
     def get_pet_by_id(self, pet_id):
-        self.cursor.execute("SELECT * FROM pets WHERE pet_id=?", pet_id)
+        self.cursor.execute("SELECT * FROM pets WHERE Licence_number=?", pet_id)
         pet = self.cursor.fetchone()
         if pet:
             return {
-                "id": pet[0],
-                "name": pet[1],
-                "breed": pet[2],
-                "age": pet[3],
-                "status": pet[4]
+                "Licence_number": pet[0],
+                "Gender": pet[1],
+                "Age": pet[2],
+                "Health_condition": pet[3],
+                "Type_":pet[4],
+                "Status_": pet[5]
             }
         return None
 
     def submit_adoption_request(self, pet_id, username, quiz_result):
         if quiz_result:
-            self.cursor.execute("UPDATE pets SET status='Adopted' WHERE pet_id=?", pet_id)
-            self.cursor.execute("UPDATE UserProfile SET status='Adopter' WHERE username=?", username)
+            self.cursor.execute("UPDATE Pets SET Status_='Adopted' WHERE Licence_number=?", pet_id)
+            # self.cursor.execute("UPDATE UserProfiles SET status='Adopter' WHERE username=?", username)
             self.conn.commit()
             return {"message": "Adoption request approved!"}
         return {"message": "Adoption request rejected"}, 400
 
     def get_adoption_updates(self, adopter_id):
-        self.cursor.execute("SELECT * FROM adoption_updates WHERE adopter_id=?", adopter_id)
+        self.cursor.execute("SELECT * FROM adoption_updates WHERE username=?", adopter_id)
         updates = self.cursor.fetchall()
         if updates:
             return [{"update_id": row[0], "message": row[1], "date": row[2]} for row in updates]
