002aef1465e3c3decdd51ba92fe7d4fa4f92c4b5
faridaali2545 <156243927+faridaali2545@users.noreply.github.com>
2024-11-30 18:50:50 +0200

    Add files via upload

---
+++ adoptionQuizService.py
@@ -0,0 +1,31 @@
+class AdoptionService:
+    def __init__(self):
+        self.questions = [
+            "How much time can you dedicate to taking care of a pet daily?",
+            "How familiar are you with pet health and care?",
+            "Are you willing to adopt a pet from a shelter?",
+            "Do you have experience with training pets?",
+            "Do you have a pet care emergency plan?",
+        ]
+        self.options = [
+            ["1-2 hours", "3-4 hours", "5+ hours", "None"],
+            ["None", "Some experience", "Moderate experience", "Expert"],
+            ["Yes", "No", "Not sure", "Already adopted from a shelter"],
+            ["None", "Basic training", "Advanced training", "Expert trainer"],
+            ["No", "Yes, I have a plan", "I am considering one", "Not sure"],
+        ]
+        self.correct_answers = [2, 2, 0, 2, 1]  # Expected correct indices
+
+    def ask_adoption_quiz(self, username, answers):
+        score = 0
+        for i, answer in enumerate(answers):
+            if answer - 1 == self.correct_answers[i]:
+                score += 1
+                if score >= 3:
+                    return True
+        return False
+
+    def choose_adoption(self, username):
+        # Assume the user opts to take the quiz
+        # This could involve other business logic if required.
+        return True
