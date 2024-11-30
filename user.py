a29a32a4e4b768ec5ddcb47e41ffd9a2b94d35cd
Azza-Monir <s-azza.abdelhamed@zewailcity.edu.eg>
2024-11-30 19:18:09 +0200

    Update user.py

--- user.py
+++ user.py
@@ -1,17 +1,39 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, render_template, redirect, url_for, flash
 from services.user_service import UserService
 
 user_bp = Blueprint("user", __name__)
 user_service = UserService()
 
-@user_bp.route("/create_user", methods=["POST"])
+@user_bp.route("/create_user", methods=["GET", "POST"])
 def create_user():
-    return user_service.create_user(request.json)
+    if request.method == "POST":
+        data = request.form.to_dict()
+        pet_data = None
+        if data.get('pet') == 'Yes':
+            pet_data = {
+                "age": data.get('age'),
+                "location": data.get('location'),
+                "breed": data.get('breed'),
+                "history": data.get('history')
+            }
+        return user_service.create_user(data, pet_data)
+    return render_template("create_user.html")
 
-@user_bp.route("/edit_user/<username>", methods=["PUT"])
+@user_bp.route("/edit_user/<username>", methods=["GET", "POST"])
 def edit_user_profile(username):
-    return user_service.edit_user_profile(username, request.json)
+    if request.method == "POST":
+        data = request.form.to_dict()
+        pet_data = None
+        if data.get('pet') == 'Yes':
+            pet_data = {
+                "age": data.get('age'),
+                "location": data.get('location'),
+                "breed": data.get('breed'),
+                "history": data.get('history')
+            }
+        return user_service.edit_user_profile(username, data, pet_data)
+    return render_template("edit_user.html", username=username)
 
-@user_bp.route("/delete_user/<username>", methods=["DELETE"])
+@user_bp.route("/delete_user/<username>", methods=["POST"])
 def delete_user_profile(username):
     return user_service.delete_user_profile(username)
