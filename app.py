a4ba89a04118baf2ea49f2962f9a17115695303e
faridaali2545 <156243927+faridaali2545@users.noreply.github.com>
2024-12-02 20:41:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,78 @@
+# from flask import Flask, send_from_directory
+# from routes.adoptionQuiz import adoption_bp
+
+# app = Flask(__name__)
+
+# # Register Blueprints
+# app.register_blueprint(adoption_bp)
+
+# # Serve static files (HTML, CSS, JavaScript)
+# @app.route("/")
+# def index():
+#     return send_from_directory("static", "quiz.html")
+
+# @app.route("/static/<path:filename>")
+# def static_files(filename):
+#     return send_from_directory("static", filename)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+###################################################################################################################
+
+# from flask import Flask, send_from_directory
+# from routes.user import user_bp  
+
+# app = Flask(__name__)
+
+# # Register the blueprint for user-related routes
+# app.register_blueprint(user_bp)
+
+# # Route to serve the HTML front-end
+# @app.route("/user.html")
+# def user_management():
+#     return send_from_directory("static", "user.html")
+
+# # Route to serve CSS and other static files (JS, images, etc.)
+# @app.route("/static/<path:filename>")
+# def static_files(filename):
+#     return send_from_directory("static", filename)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+###################################################################################################################
+
+
+# from flask import Flask
+# from adoption import adoption_bp
+
+# app = Flask(__name__)
+
+# # Register the adoption blueprint
+# app.register_blueprint(adoption_bp)
+
+# # Route to serve the HTML file
+# @app.route('/home')
+# def home():
+#     return app.send_static_file('adoption.html')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+###################################################################################################################
+
 from flask import Flask
-from config import connect_to_database
+from marketPlace import marketplace_bp  
+# from routes.petActivity import pet_activity_bp  
 
 app = Flask(__name__)
 
-conn = connect_to_database()
-cursor = conn.cursor()
+# # Register the marketplace routes blueprint
+app.register_blueprint(marketplace_bp, url_prefix='/api/marketplace')
 
-if __name__ == "main":
+# Register the pet activity routes blueprint
+# app.register_blueprint(pet_activity_bp, url_prefix='/api/activities')
+
+if __name__ == "__main__":
     app.run(debug=True)
+###################################################################################################################
