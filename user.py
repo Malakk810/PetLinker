8e8f7b8fcd381a0c89fd5dac2222706de1c4ff60
Azza-Monir <s-azza.abdelhamed@zewailcity.edu.eg>
2024-12-02 17:30:59 +0200

    Update user.py

--- user.py
+++ user.py
@@ -1,32 +1,28 @@
-from flask import Blueprint, request, jsonify
-from services.user_service import UserService
+from flask import Blueprint, request, jsonify   #import flask tools
+from services.user_service import UserService   #import class container
+#user is the container for all routes
+user= Blueprint('user', __name__)  #group routes
+user_service = UserService()  #class handle all business logic & user actions
 
-user_bp = Blueprint('user', __name__)
-user_service = UserService()
-
-# User Registration Route (Create new account)
-@user_bp.route("/user/register", methods=["POST"])
+@user.route("/user/register", methods=["POST"])    #route that handles post requests
 def register_user():
-    data = request.json
-    result = user_service.create_user(data)
-    return jsonify(result), 201 if result["success"] else 400
+    data = request.json  #extract the data from the request body
+    result = user_service.create_user(data)   #authenticate data entered by user
+    return jsonify(result), 201 if result["success"] else 400   #201 for POST method
 
-# User Login Route
-@user_bp.route("/user/login", methods=["POST"])
+@user.route("/user/login", methods=["POST"])
 def login_user():
     data = request.json
     result = user_service.login_user(data)
-    return jsonify(result), 200 if result["success"] else 400
+    return jsonify(result), 200 if result["success"] else 400   #200 for GET method
 
-# Edit Account Route
-@user_bp.route("/user/edit/<string:username>", methods=["PUT"])
+@user.route("/user/edit/<string:username>", methods=["PUT"])
 def edit_user(username):
     data = request.json
     result = user_service.edit_user_profile(username, data)
-    return jsonify(result), 200 if result["success"] else 400
+    return jsonify(result), 200 if result["success"] else 400 #400 for unsuccessful
 
-# Delete Account Route
-@user_bp.route("/user/delete/<string:username>", methods=["DELETE"])
+@user.route("/user/delete/<string:username>", methods=["DELETE"])
 def delete_user(username):
     result = user_service.delete_user_profile(username)
     return jsonify(result), 200 if result["success"] else 400
