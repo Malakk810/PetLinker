673545081d6f80811e856fbb23879d5414c029f6
Azza-Monir <s-azza.abdelhamed@zewailcity.edu.eg>
2024-12-02 21:20:57 +0200

    Update user.py

--- user.py
+++ user.py
@@ -1,48 +1,31 @@
 from flask import Blueprint, request, jsonify   #import flask tools
 from services.user_service import UserService   #import class container
-#user is the container for all routes
-user= Blueprint('user', __name__)  #group routes
-user_service = UserService()  #class handle all business logic & user actions
+# user is the container for all routes
+user = Blueprint('user', __name__)  # group routes
+user_service = UserService()  # class handle all business logic & user actions
 
-@user.route("/user/register", methods=["POST"])    #route that handles post requests
+@user.route("/user/register", methods=["POST"])  # route that handles post requests
 def register_user():
-    try:
-        data = request.json  #extract the data from the request body
-        result = user_service.create_user(data)   #authenticate data entered by user
-        return jsonify(result), 201 if result["success"] else 400   #201 for POST method
-    except Exception as error:   #catches errors and save it into 'error'
-        return jsonify({"success": False, "message": str(error)}), 500  #converts error into string for users
-
+    data = request.json  # extract the data from the request body
+    result = user_service.create_user(data)  # authenticate data entered by user
+    return jsonify(result), 201 if result["success"] else 400  # 201 for POST method
 
 @user.route("/user/login", methods=["POST"])
 def login_user():
-    try:
-        data = request.json
-        result = user_service.login_user(data)
-        return jsonify(result), 200 if result["success"] else 400   #200 for GET method
-    except Exception as error:
-        return jsonify({"success": False, "message": str(error)}), 500
-
+    data = request.json
+    result = user_service.login_user(data)
+    return jsonify(result), 200 if result["success"] else 400  # 200 for successful login
 
 @user.route("/user/edit/<string:username>", methods=["PUT"])
 def edit_user(username):
-    try:
-       data = request.json
-       result = user_service.edit_user_profile(username, data)
-       return jsonify(result), 200 if result["success"] else 400 #400 for unsuccessful
-    except Exception as error:
-        return jsonify({"success": False, "message": str(error)}), 500
-
-        
-
+    data = request.json
+    result = user_service.edit_user_profile(username, data)
+    return jsonify(result), 200 if result["success"] else 400  # 400 for unsuccessful update
 
 @user.route("/user/delete/<string:username>", methods=["DELETE"])
 def delete_user(username):
-    try:
-       result = user_service.delete_user_profile(username)
-       return jsonify(result), 200 if result["success"] else 400
-    except Exception as error:
-        return jsonify({"success": False, "message": str(error)}), 500
+    result = user_service.delete_user_profile(username)
+    return jsonify(result), 200 if result["success"] else 400
 
 
 
