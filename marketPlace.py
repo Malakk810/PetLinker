049e6989ed8e4d6e5f1042ec2d8c1dfcb93ef110
nadaaZC <147438932+nadaaZC@users.noreply.github.com>
2024-12-02 21:45:33 +0200

    Update marketPlace.py

--- marketPlace.py
+++ marketPlace.py
@@ -7,21 +7,22 @@ marketplace_service = MarketplaceService()
 
 @marketplace_bp.route("/marketplaces", methods=["GET"])
 def get_all_pet_marketplaces():
-#     # Get location from query parameters
-#     location = request.args.get('location')
-    
-#     # Get the pet marketplaces from the service
-#     marketplaces = marketplace_service.get_pet_marketplaces(location)
-    
-#     if isinstance(marketplaces, str):  # Error message case
-#         return jsonify({"message": marketplaces}), 404
+    # Get location from query parameters
+    location = request.args.get('location')
 
-#     return jsonify(marketplaces)
-    return "marketplace"
+    # Get the pet marketplaces from the service
+    marketplaces = marketplace_service.get_pet_marketplaces(location)
+
+    if isinstance(marketplaces, str):  # Error message case
+        return jsonify({"message": marketplaces}), 404
+
+    return jsonify(marketplaces)
 
 @marketplace_bp.route("/marketplaces/sample", methods=["POST"])
 def insert_sample_marketplaces():
-    # Insert the sample data into the database
-    marketplace_service.insert_sample_data()
-    return jsonify({"message": "Sample pet marketplaces inserted successfully!"}), 201
-
+    try:
+        # Insert the sample data into the database
+        marketplace_service.insert_sample_data()
+        return jsonify({"message": "Sample pet marketplaces inserted successfully!"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
